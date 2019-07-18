126004fe7f054fda99cc35451fb40617205a0d5c
Milton Pauta <>
2019-07-18 15:48:44 -0400

    post register, new user can now be created

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,12 +1,26 @@
+const User = require('../models/User')
+
 //login user! 
 exports.postLogin=(req,res,next)=>{
 
 }
 
 exports.postRegister = (req, res, next)=>{
-    res.status(200).json({
-        message: 'success'
-    }); 
+    const email = req.body.email; 
+    const password = req.body.password; 
+
+    const newUser = new User(email, password); 
+    newUser.save()
+    .then(result=>{
+         res.status(200).json({
+            message: 'User created' 
+         })
+    })
+    .catch(err=>{
+        const error = new Error('Error with creating user.')
+        error.status(400); //see if this correct error code 
+        next(error)
+    })
 }
 
 exports.postLogout = (req, res, next)=>{
---
+++ api/models/User.js
@@ -0,0 +1,47 @@
+const getDb = require('../../utils/database').getDb;
+const mongo = require('mongodb')
+
+class User {
+    //add file next, userID will be added last 
+    constructor(email,password){
+        this.email = email; 
+        this.password = password; 
+    }
+
+    save(){
+        const db = getDb();
+        return db.collection('users').insert(this)
+        .catch(err=>{
+            console.log(err); 
+        })
+    }
+
+    static findUserById(id){
+        const db = getDb();
+        return db.collection('users')
+        .find({_id: new mongo.ObjectId(id)})
+        .next()
+        .then(foundUser=>{
+            return foundUser; 
+        })
+        .catch(err=>{
+            console.log(err);  
+        })
+    }
+
+    //make a query that retrieves all posts from collection 
+    static fetchAllUsers(){
+        const db = getDb();
+        return db.collection('users').find()
+        .toArray() 
+        .then(allUsers=>{
+            return allUsers;  
+        })
+        .catch(err=>{
+            console.log(err);  
+        })
+    }
+
+}
+
+module.exports = User; 
