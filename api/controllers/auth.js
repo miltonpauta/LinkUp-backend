97b13a45fa480af9502768dee39020e83376293c
Milton Pauta <>
2019-07-18 23:46:40 -0400

    validation for register route created and working

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,18 +1,26 @@
 const User = require('../models/User')
 
+const {validationResult} = require('express-validator'); 
+
 //login user! 
 exports.postLogin=(req,res,next)=>{
 
 }
 
 exports.postRegister = (req, res, next)=>{
+    //get validation error 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
     const email = req.body.email; 
     const password = req.body.password; 
 
     const newUser = new User(email, password); 
     newUser.save()
     .then(result=>{
-         res.status(200).json({
+         return res.status(200).json({
             message: 'User created' 
          })
     })
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,11 +4,24 @@ const router = express.Router();
 
 const AuthController = require('../controllers/auth')
 
+const {check} = require('express-validator/check'); 
+
 //login 
 router.post('/login', AuthController.postLogin); 
 
-//sign up 
-router.post('/register', AuthController.postRegister); 
+//sign up -- ADDING VALIDATION HERE 
+router.post('/register', [
+    check('email')
+    .isEmail().withMessage('Please enter a valid email!')
+    .normalizeEmail(),
+
+    check('password', 'The password must be 5+ chars long and contain a number')
+    .not().isIn(['123', 'password', 'god']).withMessage('Do not use a common word as the password')
+    .isLength({ min: 5 })
+    .matches(/\d/)
+    ],
+    AuthController.postRegister
+); 
 
 //sign out 
 router.post('/logout', AuthController.postLogout); 
